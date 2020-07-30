fc1e564fae1fad7e468eea1845118bba36105c42
The Holy Ghost <holyghost@ip-172-31-85-48.ec2.internal>
2020-07-30 15:58:19 +0000

    initial commit

---
+++ parsingobject.scm
@@ -0,0 +1,15 @@
+(load "tokenizer.scm") ;; from gondolinutil
+
+(define (make-parsing-object object)
+	(let* ((*rawvalue object))
+		)
+
+	(define (get)
+		*rawvalue)
+
+	(define (dispatch msg)
+		(cond ((eq? msg 'get) get)
+
+			(else (display "make-parsing-object : message not understood : ")(display msg)(newline)
+			)))
+	))
